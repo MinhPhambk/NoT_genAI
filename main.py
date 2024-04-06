f8247377996f1493b65f941b5630a9ae7160b854
Nhat Minh <98695231+NDNM1408@users.noreply.github.com>
2024-04-06 19:14:40 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI, File, UploadFile
 from read_pdf import read_pdf
-
+from llama_parse import LlamaParse
+import asyncio
+import nest_asyncio
 app = FastAPI()
+nest_asyncio.apply()
 
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
     with open(file.filename, "wb") as buffer:
         buffer.write(await file.read())
+
     text = read_pdf(file.filename)
     return {"filename": file.filename,
-            "content": text}
+                "content": text}
--- read_pdf.py
+++ read_pdf.py
@@ -1,9 +1,8 @@
 from pypdf import PdfReader
+from llama_parse import LlamaParse
+import asyncio
 
 def read_pdf(path: str):
-        
-    reader = PdfReader(path)
-    res = ""
-    for page in reader.pages:
-        res += page.extract_text()
-    return res
+    parser = LlamaParse(api_key='llx-UNeRjwphgBNvFTY2H63Hr9YnA0q98z5XTZpo33QJ5fx6hQaJ', result_type="markdown")
+    documents = parser.load_data(path)
+    return documents[0].text
